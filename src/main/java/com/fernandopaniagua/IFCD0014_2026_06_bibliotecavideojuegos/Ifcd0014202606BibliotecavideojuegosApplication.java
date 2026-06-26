1578498dc4a15cd7a6d8e1c5b494d6db6bf210b5
Fernando Paniagua <fernando.paniagua.java@gmail.com>
2026-06-26 17:37:20 +0200

    Initial commit

---
+++ src/main/java/com/fernandopaniagua/IFCD0014_2026_06_bibliotecavideojuegos/Ifcd0014202606BibliotecavideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.IFCD0014_2026_06_bibliotecavideojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ifcd0014202606BibliotecavideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ifcd0014202606BibliotecavideojuegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandopaniagua/IFCD0014_2026_06_bibliotecavideojuegos/Ifcd0014202606BibliotecavideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.IFCD0014_2026_06_bibliotecavideojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ifcd0014202606BibliotecavideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
