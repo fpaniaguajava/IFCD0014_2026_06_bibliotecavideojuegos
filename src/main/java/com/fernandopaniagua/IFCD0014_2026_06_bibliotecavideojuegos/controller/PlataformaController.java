89b5d78aa09f605f1265baffcc9921e170569fcf
Fernando Paniagua <fernando.paniagua.java@gmail.com>
2026-07-02 17:00:22 +0200

    Creación de plataformas

--- src/main/java/com/fernandopaniagua/IFCD0014_2026_06_bibliotecavideojuegos/controller/PlataformaController.java
+++ src/main/java/com/fernandopaniagua/IFCD0014_2026_06_bibliotecavideojuegos/controller/PlataformaController.java
@@ -1,18 +1,39 @@
 package com.fernandopaniagua.IFCD0014_2026_06_bibliotecavideojuegos.controller;
 
+import com.fernandopaniagua.IFCD0014_2026_06_bibliotecavideojuegos.model.Plataforma;
+import com.fernandopaniagua.IFCD0014_2026_06_bibliotecavideojuegos.service.IPlataformaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PlataformaController {
-   @GetMapping("/plataforma/nueva")
-   public String crearPlataforma(Model model){
-       return "crear-plataforma";
-   }
+    private IPlataformaService plataformaService;
 
-   @GetMapping("/plataforma/lista")
-   public String mostrarPlataformas(Model model){
-       return "listado-plataformas";
-   }
+    public PlataformaController(IPlataformaService plataformaService){
+        this.plataformaService = plataformaService;
+    }
+
+    @GetMapping("/plataforma/nueva")
+    public String crearPlataforma(Model model) {
+        return "crear-plataforma";
+    }
+
+    @GetMapping("/plataforma/lista")
+    public String mostrarPlataformas(Model model) {
+        return "listado-plataformas";
+    }
+
+    @PostMapping("/plataformas/guardar")
+    public String guardarPlataforma(
+            @RequestParam String fabricante,
+            @RequestParam String nombre,
+            @RequestParam int anyo,
+            Model model) {
+        Plataforma plataforma = new Plataforma(fabricante, nombre, anyo);
+        this.plataformaService.create(plataforma);
+        return "index";
+    }
 }
